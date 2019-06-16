5083c89c6d454f1a739db7d4c2dd9d4f9f0102a6
dcaozhhmac <860712710@qq.com>
2019-06-16 21:26:02 +0800

    views01

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 def index(request):
     # 测试程序
     return HttpResponse("Hello, World. Your are at the polls index.")
+
+def detail(request,qustion_id):
+    return  HttpResponse('You are looking at question %s.' %qustion_id)
+
+def results(request, qustion_id):
+    response = "You're looking at the results of question %s."
+    return  HttpResponse(response %qustion_id)
+
+def vote(request,qustion_id):
+    return HttpResponse("You're voting on question %s." %qustion_id)
