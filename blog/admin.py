44b65a22194f785604fa3db755aabbd83144b229
dcaozhhmac <860712710@qq.com>
2019-07-08 23:13:16 +0800

    blog model

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import BlogPost
 
 # Register your models here.
-admin.site.register(BlogPost)
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+
+
+admin.site.register(BlogPost, BlogPostAdmin)
