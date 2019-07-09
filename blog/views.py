e338b5eff33825fee9234b0663ae880d95b0b87a
dcaozhhmac <860712710@qq.com>
2019-07-09 22:26:20 +0800

    blog views1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from datetime import  datetime
+from django.shortcuts import render_to_response
+from blog.models import BlogPost
 
 # Create your views here.
+def archive(request):
+    post = BlogPost(title='mocktitle', body='mockbody', timestamp=datetime.now())
+    return render_to_response('blog/archive.html', {'posts': [post]})
