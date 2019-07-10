73c549460a3097fd65c8825e568cdc5cf1f4956e
dcaozhhmac <860712710@qq.com>
2019-07-10 22:53:20 +0800

    input1

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,13 @@ from datetime import  datetime
 from django.shortcuts import render_to_response
 from blog.models import BlogPost
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 # Create your views here.
 def archive(request):
     # post = BlogPost(title='mocktitle', body='mockbody', timestamp=datetime.now())
     posts = BlogPost.objects.all()[:10]
-    return render_to_response('blog/archive.html', {'posts': posts})
+    return render_to_response('blog/archive.html', {'posts': posts}, RequestContext(request))
 
 
 def create_blogpost(request):
